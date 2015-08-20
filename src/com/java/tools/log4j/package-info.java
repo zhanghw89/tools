841686d6ef6f9d887d26907baeacbab3a0bf0463
zhanghw <1803794281@qq.com>
2015-08-20 11:57:13 +0800

    init project

---
+++ src/com/java/tools/log4j/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+
+/**   
+ * @Description    TODO
+ * @author         zhanghw@chinatelecom.com.cn
+ * @since          2015年8月20日
+ * @version        V1.0
+ */
+
+package com.java.tools.log4j;
