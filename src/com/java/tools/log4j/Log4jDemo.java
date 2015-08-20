73b996851fec22f2d647daa2cbfcdc3a6e638500
zhanghw <1803794281@qq.com>
2015-08-20 13:20:03 +0800

    log4j

--- src/com/java/tools/log4j/Log4jDemo.java
+++ src/com/java/tools/log4j/Log4jDemo.java
@@ -1,6 +1,15 @@
 package com.java.tools.log4j;
 
-public class Log4jDemo {
-//	private static final Logger logger = Logger
+import org.apache.log4j.Logger;
 
+
+public class Log4jDemo {
+	private static final Logger logger = Logger.getLogger(Log4jDemo.class);
+	
+	public static void main(String[] args) {
+		logger.info("info...");
+		logger.debug("debug...");
+		logger.error("error...");
+	}
+	
 }
